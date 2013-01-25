2806c256109fd392fd73d8691d33f090a92ab7bc
Andrzej Undziłło (s4694) <s4694@pjwstk.edu.pl>
2013-01-25 15:44:27 +0100

    Add Book attribs presence and length validations

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,6 @@
 class Book < ActiveRecord::Base
+  validates :author, :presence => true, :length => { :in => 3..255 }
+  validates :title, :presence => true, :length => { :in => 1..255 }
+
   attr_accessible :author, :title
 end
