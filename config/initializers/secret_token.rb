1299e6dd18d2d42185de1a9143ab1278aebcb29c
Andrzej Undziłło (s4694) <s4694@pjwstk.edu.pl>
2013-01-25 12:28:44 +0100

    Initial empty Rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HerokuDemo::Application.config.secret_token = '225ab9de7c4d4f552dcd7c0d2e00b9790c48d696defdf19d10d47e90e0fecd8b64f5b5ccaf0f0c267e7e4e15dbdc436aefecb5aa5131f74d057dc7265ed05c4f'
